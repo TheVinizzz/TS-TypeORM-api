b0143173d71620077b1a631fc04aca9480b855f3
Carlos Vinicius <viniprograming@gmail.com>
2021-01-24 11:20:16 -0300

    feature heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,7 @@
 console.log('process.env.DATABASEE_URL :>> ', process.env.API_ROUTE_AUTHENTICATE);
 module.exports = {
   "type": "postgres",
-  "host": process.env.API_ROUTE_AUTHENTICATE,
-  "port": process.env.API_ROUTE_PORT,
-  "username": process.env.API_USER,
-  "password": process.env.API_PASSWORD,
-  "database": process.env.API_DATABASE,
+  "url": process.env.API_ROUTE_AUTHENTICATE,
   "entities": [
     "dist/models/**/*.js"
  ],
